0bb82bd03ac92f9329398bda0f1822ba6bbd7b13
RahilSheikh11 <rahilsheikh012@gmail.com>
2026-04-17 00:04:25 +0530

    main

--- App.js
+++ App.js
@@ -2,17 +2,21 @@
 // const root =ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
 
-const heading = React.createElement("h1", 
-    {id: "Parent"},[
-     React.createElement("h6", 
-    {id: "Child"},"I am child" ),
-    React.createElement("h6", 
-    {id: "Child2"},"I am child2" )
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement("div", 
+    {id: "Parent"},
+    React.createElement("div",{id: "child"}, [
+     React.createElement("h1",{key: "1", 
+    id: "Child"},"I am Namaste React 🚀" ),
+    React.createElement("h6",{key: "2" ,
+    id: "Child2"},"I am child2" )
     ,
-    React.createElement("h6", 
-    {id: "Child3"},["I am child3", "11" ,"22","33"])
+    React.createElement("h6", {key: "3",
+    id: "Child3"},["I am child3", "11" ,"22","33"])
     
-     ]
+     ])
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
---
+++ README.md
@@ -0,0 +1,19 @@
+#Parcel
+- Dev Build
+- Local Server
+- HMR = Hot module replacement
+- File Watching Algorithm - written in C++
+- Caching - Faster builds
+- Image optimization
+- Minification of files
+- Bundles 
+- Compresses the files
+- Consistent Hashing (Read about it)
+- Code spilitting
+- Differential bundling - To support older browser
+- Diagnostics
+- Error Handling
+- HTTPs
+- Tree Shaking algorithm - removes unused code for us
+- Different Dev and Produ bundles
+
